b1f7aee8e0011718d6095d5e558f7f7f714c4aaf
Chien <toanhoc9xx@gmail.com>
2025-11-12 22:59:10 +0700

    Đẩy file tĩnh lên cloud-v2

--- routes/admin/product-route.js
+++ routes/admin/product-route.js
@@ -1,18 +1,8 @@
 const express = require ('express');
 const multer  = require('multer')
-const cloudinary = require('cloudinary').v2
-const streamifier = require('streamifier')
-// const storageMulter = require("../../helpers/storageMulter")
-
-
-cloudinary.config({ 
-        cloud_name: 'daksmfztr', 
-        api_key: '637692585737827', 
-        api_secret: 'PFtQ7yFujF2Tib-dj1snXGrhnIo' 
-});
 
 const upload = multer()
-
+const uploadCloud = require("../../middlewares/admin/uploadCloud-middleware")
 const route = express.Router();
 const controller = require("../../controllers/admin/product-controller")
 const validate = require("../../validates/admin/product-validate")
@@ -30,33 +20,7 @@ route.get("/create",controller.create)
 route.post(
     "/create",
     upload.single("thumbnail"),
-    function (req, res, next) {
-        if(req.file){
-            let streamUpload = (req) => {
-                return new Promise((resolve, reject) => {
-                    let stream = cloudinary.uploader.upload_stream(
-                        (error, result) => {
-                        if (result) {
-                            resolve(result);
-                        } else {
-                            reject(error);
-                        }
-                        }
-                    );
-
-                    streamifier.createReadStream(req.file.buffer).pipe(stream);
-                });
-            };
-            async function upload(req) {
-                let result = await streamUpload(req);
-                req.body[req.file.fieldname] = result.secure_url;
-                next();
-            }
-            upload(req);
-        }else{
-            next();
-        }
-    },
+    uploadCloud.upload,
     validate.createPost, 
     controller.createPost)
 
